68624c8d5790f8a63eed7b0da41355a60044cf09
NaveenDK <naveenkolambage@gmail.com>
2022-10-29 12:48:45 +1300

    working v1

--- backend/routes/cycles.js
+++ backend/routes/cycles.js
@@ -11,58 +11,30 @@ router.route('/').get((req,res)=>{
 
 
 router.route('/add').post((req,res)=>{
-// let fullname=req.body.firstname+req.body.lastname
-// const username = fullname;
-console.log("add reached")
-const startDate= Date.parse ( req.body.startDate) 
-const endDate=  Date.parse ( req.body.endDate) 
-//const goals=req.body.users.goals
+ 
+        console.log("add reached")
+        const startDate= Date.parse ( req.body.startDate) 
+        const endDate=  Date.parse ( req.body.endDate) 
+        
 
-const users=req.body.users
+        const users=req.body.users
 
 
-const newCycle=new Cycle({
-    startDate,endDate,users
+        const newCycle=new Cycle({
+            startDate,endDate,users
+        })
+        newCycle.save()
+        .then(()=>res.json('Cycle Added' ))
+        .catch(err=>res.status(400).json('Error '+err))
+
 })
-
 router.route('/:id').get((req,res)=>{
     
     Cycle.findById(req.params.id)
     .then(cycle=>res.json(cycle))
     .catch(err=>res.status(400).json('Error:' + err) )
 }) 
-// router.get('/:id', (req,res)=>{
-    
-//     Cycle.findById(req.params.id)
-//     .then(cycle=>res.json(cycle))
-//     .catch(err=>res.status(400).json('Error:' + err))
-// })
-// router.get('/:id', (req, res, next) => {
-
-//     Cycle.findById(req.params.id)
-//       .then(cycle => {
-//         if (!cycle) {
-//           return next(console.log(`Cycle with id ${req.params.id} not found`))
-//         } else {
-//           return res.json(cycle)
-//         }
-//       })
-//       .catch(err => res.status(400).json('Error:' + err))
-//   })
-// router.route('/:id').get((req,res)=>{
-//     var ObjectId = require('mongodb').ObjectId;
-//  console.log("HII")
-
-//     Cycle.findById(new ObjectId(req.params.id))
-//         .then(cycle=>res.json(cycle))
-//         .catch(err=>res.status(400).json('Error:' + err))    
-        
-//     })
-
-
-// router.route('/:id').get((req,res)=>{
-//     console.log("CHECK")
-// })
+  
 
 router.route('/:id').delete((req,res)=>{
     Cycle.findByIdAndDelete(req.params.id)
@@ -76,11 +48,53 @@ router.route('/update/:id').post((req,res)=>{
         cycle.startDate = req.body.startDate
         cycle.endDate=req.body.endDate
         cycle.users=req.body.users
+
+        
+     cycle.save()
+     .then (()=>res.json('Cycle updated'))
+     .catch(err=>res.status(400).json('Error'+err))
      })
+
+
 })
-newCycle.save()
-.then(()=>res.json('Cycle Added' ))
-.catch(err=>res.status(400).json('Error '+err))
-})
+// router.route('/updatetask/:cycleId/user/:userId/task/:taskId/subtask/:sbId').post((req,res)=>{
+   
+//     console.log("req.params.userId")
+//      console.log(req.params.userId)
+//      console.log("req.params.taskId")
+//      console.log(req.params.taskId)
+     
+//      Cycle.findById(req.params.cycleId)
+//      .then(cycle=>{
+       
+//       //console.log("cycle : "+JSON.stringify(cycle))
+      
+//       console.log("typeof cycle.users : "+ typeof cycle.users)
+//       let user = cycle.users.find(item => item._id == req.params.userId);
+//       let goal= user.goals.find(goal=>goal._id==req.params.taskId)
+//       //let sb= user.goals.subtasks.find(goal=>goal._id==req.params.taskId)
+//       let subtask= goal.subTasks.find(sb=>sb._id==req.params.sbId)
+//     //   console.log("user found: " + user)
+//     //   console.log("goal found: " + goal)
+//     //   console.log("Subtaskfound: " + subtask)
+//     //   console.log("subtask.done" +subtask.done )
+//     //   subtask.done=req.body.us
+//     console.log("check whats in req.body" + JSON.stringify(req.body.users.userId))
+//       cycle.save()
+//       .then (()=>res.json('cycle updated'))
+//       .catch(err=>res.status(400).json('Error'+err))
+//      // console.log("Subtask found: " + subtask)
+//     ///  console.log(req.params.userId)
+//      })
+  
+// })
+// router.route('/update/:cycleId/user/:userId/task/:taskId/subtask/:subtaskId').get((req,res)=>{
+//     Cycle.findById(req.params.cycleId)
+//      .then(cycle=>{
+         
+//        console.log("print cycle: "+ cycle)
+//      })
+// })
+
 
 module.exports = router
