4f83b2790e873a2dd8f252d74a2e6416b8781d7e
NaveenDK <naveenkolambage@gmail.com>
2023-05-04 03:52:27 +1200

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,9 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 } else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
 }
 ///---------deployment --------------------------
 
